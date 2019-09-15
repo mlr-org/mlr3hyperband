d9337b7ceb5ddce2c54bf1e8bf2a6a33bbcbd0ef
SebGruber1996 <33655584+SebGruber1996@users.noreply.github.com>
2019-09-15 11:28:02 +0200

    fixed budget print info

--- test.R
+++ test.R
@@ -1,5 +1,6 @@
 devtools::load_all()
 library(mlr3learners)
+library(mlr3tuning)
 library(data.table)
 
 set.seed(123)
@@ -8,7 +9,7 @@ lg$set_threshold("error")
 # define hyperparameter and budget parameter for tuning with hyperband
 ps = ParamSet$new(list(
 
-  ParamInt$new("nrounds",           lower = 0.01, upper = 0.81, tag = "budget"),
+  ParamInt$new("nrounds",           lower = 1, upper = 81, tag = "budget"),
   ParamDbl$new("eta",               lower = 0, upper = 1),
   ParamInt$new("num_parallel_tree", lower = 1, upper = 100),
   ParamInt$new("max_depth",         lower = 1, upper = 100),
@@ -42,10 +43,10 @@ tuner$info[, .(
   bracket_stages = max(bracket_stage),
   b_start = min(budget),
   b_end = max(budget),
-  b_sum = sum(budget * mu),
+  b_sum = sum(budget),
   b_real_start = min(budget_real),
   b_real_end = max(budget_real),
-  b_real_sum = sum(budget_real * mu),
+  b_real_sum = sum(budget_real),
   mu_start = max(mu),
   mu_end = min(mu)
 ), by = bracket]
