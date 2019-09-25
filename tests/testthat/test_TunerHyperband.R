172a7cf2b5ade54597c31885a4a38e9f3d2e92d8
SebGruber1996 <gruber_sebastian@t-online.de>
2019-09-25 16:44:01 +0200

    added unit test for subsampling

--- tests/testthat/test_TunerHyperband.R
+++ tests/testthat/test_TunerHyperband.R
@@ -16,3 +16,34 @@ test_that("TunerHyperband multicrit", {
   test_tuner("hyperband", eta = 2L, lower_b = 1, upper_b = 16, real_evals = 16, n_dim = 2L, measures = c("classif.fpr", "classif.tpr"))
 
 })
+
+
+test_that("TunerHyperband using subsampling", {
+
+  library(mlr3pipelines)
+  set.seed(123)
+
+  # define hyperparameter for tuning with hyperband
+  params = list(
+    ParamDbl$new("cp", lower = 0.001, upper = 0.1),
+    ParamInt$new("minsplit", lower = 1, upper = 10)
+  )
+
+
+  inst = TuningInstance$new(
+    tsk("iris"),
+    lrn("classif.rpart"),
+    rsmp("holdout"),
+    msr("classif.ce"),
+    ParamSet$new(params),
+    term("evals", n_evals = 100000)
+  )
+
+
+  tuner = TunerHyperband$new(eta = 2L, use_subsamp = TRUE)
+  tuner$tune(inst)
+
+  results = inst$archive()[, c("cp", "minsplit", "classif.ce")]
+
+  expect_data_table(results, ncols = 3, nrows = 35)
+})
