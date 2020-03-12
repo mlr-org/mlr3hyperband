ca0e6a9f9caff5cbd1e726f18a8bbf40a7eaa911
pat-s <patrick.schratz@gmail.com>
2020-03-12 09:21:35 +0100

    remove travis

--- tic.R
+++ tic.R
@@ -3,7 +3,7 @@ do_package_checks(error_on = "error")
 get_stage("install") %>%
   add_step(step_install_cran("bibtex"))
 
-if (ci_has_env("BUILD_PKGDOWN")) {
+if (ci_on_ghactions()) {
   get_stage("install") %>%
     add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown()
