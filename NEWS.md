cded7fbc892817fef457fe348cd00efc12f54c83
Marc Becker <marcbecker@posteo.de>
2020-09-03 13:43:17 +0200

    Cleanup 2

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,3 @@
 # mlr3hyperband 0.1.0
 
-* Initial prototype.
+* Initial CRAN release.
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,12 +1,5 @@
-lapply(
-  list.files(
-    system.file("testthat", package = "mlr3"),
-    pattern = "^helper.*\\.[rR]$",
-    full.names = TRUE
-  ),
-  source
-)
-
+lapply(list.files(system.file("testthat", package = "mlr3"),
+  pattern = "^helper.*\\.[rR]$", full.names = TRUE), source)
 
 # calculating bracket meta info for given R and eta based on hb paper
 hyperband_brackets = function(R, eta) {
@@ -15,22 +8,17 @@ hyperband_brackets = function(R, eta) {
   B = (smax + 1) * R
 
   for (s in smax:0) {
-
     n = ceiling((B / R) * ((eta^s) / (s + 1)))
     r = R * eta^(-s)
 
     for (i in 0:s) {
-
       ni = floor(n * eta^(-i))
       ri = r * eta^i
       result = rbind(result, c(s, i, ri, ni))
     }
   }
-
-  names(result) = c("bracket", "bracket_stage", "budget_scaled", "n_configs")
-  return(result)
+  set_names(result, c("bracket", "bracket_stage", "budget_scaled", "n_configs"))
 }
-
 
 expect_tuner = function(tuner) {
   expect_r6(tuner, "Tuner",
@@ -45,7 +33,7 @@ expect_info = function(eta, lower_b, upper_b, archive) {
   hb_meta_info = hyperband_brackets(R = upper_b / lower_b, eta = eta)
   hb_meta_info = as.data.table(hb_meta_info)
   tuner_info = archive$data()[, colnames(hb_meta_info), with = FALSE]
-  tuner_info = unique(tuner_info) #becasue info for all x is duplicated in each bracket
+  tuner_info = unique(tuner_info) # info for all x is duplicated in each bracket
   real_evals = sum(tuner_info$n_configs)
 
   expect_equal(hb_meta_info, tuner_info)
@@ -54,9 +42,12 @@ expect_info = function(eta, lower_b, upper_b, archive) {
 }
 
 # single crit test
-test_tuner_hyperband = function(eta, n_dim = 1L, term_evals = NULL, lower_b, upper_b, measures = "classif.ce", learner = lrn("classif.xgboost"), task = tsk("pima"), ps = NULL) {
+test_tuner_hyperband = function(eta, n_dim = 1L, term_evals = NULL, lower_b,
+  upper_b, measures = "classif.ce", learner = lrn("classif.xgboost"),
+  task = tsk("pima"), ps = NULL) {
 
   # run for an (almost) arbitrary time if NULL is given
+
   if (is.null(term_evals)) {
     term = trm("none")
   } else {
@@ -66,12 +57,14 @@ test_tuner_hyperband = function(eta, n_dim = 1L, term_evals = NULL, lower_b, upp
   if (is.null(ps)) {
     if (n_dim == 1) {
       ps = ParamSet$new(params = list(
-        ParamInt$new("nrounds", lower = lower_b, upper = upper_b, tags = "budget"),
+        ParamInt$new("nrounds", lower = lower_b, upper = upper_b,
+          tags = "budget"),
         ParamInt$new("max_depth", lower = 1, upper = 100)
       ))
     } else if (n_dim == 2) {
       ps = ParamSet$new(params = list(
-        ParamInt$new("nrounds", lower = lower_b, upper = upper_b, tags = "budget"),
+        ParamInt$new("nrounds", lower = lower_b, upper = upper_b,
+          tags = "budget"),
         ParamDbl$new("eta", lower = 0, upper = 1),
         ParamInt$new("max_depth", lower = 1, upper = 100)
       ))
@@ -79,9 +72,13 @@ test_tuner_hyperband = function(eta, n_dim = 1L, term_evals = NULL, lower_b, upp
   }
 
   if (length(measures) == 1) {
-    inst = TuningInstanceSingleCrit$new(task, learner, rsmp("holdout"), msr(measures), ps, term)
+    inst = TuningInstanceSingleCrit$new(
+      task, learner, rsmp("holdout"),
+      msr(measures), ps, term)
   } else {
-    inst = TuningInstanceMultiCrit$new(task, learner, rsmp("holdout"), lapply(measures, msr), ps, term)
+    inst = TuningInstanceMultiCrit$new(
+      task, learner, rsmp("holdout"),
+      lapply(measures, msr), ps, term)
   }
 
   tuner = tnr("hyperband", eta = eta)
@@ -89,6 +86,7 @@ test_tuner_hyperband = function(eta, n_dim = 1L, term_evals = NULL, lower_b, upp
 
   tuner$optimize(inst)
   archive = inst$archive
+
   # compare results with full hyperband brackets if tuner was fully evaluated
   if (!inst$is_terminated) {
     expect_info(eta, lower_b, upper_b, archive)
@@ -115,16 +113,17 @@ test_tuner_hyperband = function(eta, n_dim = 1L, term_evals = NULL, lower_b, upp
 
 # test an implemented subclass tuner by running a test with dependent params
 # returns: tune_result and instance
-test_tuner_hyperband_dependencies = function(eta, term_evals = NULL, lower_b, upper_b) {
+test_tuner_hyperband_dependencies = function(eta, term_evals = NULL, lower_b,
+  upper_b) {
   ll = LearnerRegrDepParams$new()
   ll$param_set$add(
     ParamInt$new("nrounds", lower = lower_b, upper = upper_b, tags = "budget")
   )
-  test_res = test_tuner_hyperband(eta = eta, term_evals = term_evals, lower_b = lower_b, upper_b = upper_b, measures = "regr.mse", learner = ll, task = tsk("boston_housing"), ps = ll$param_set)
+  test_res = test_tuner_hyperband(eta = eta, term_evals = term_evals,
+    lower_b = lower_b, upper_b = upper_b, measures = "regr.mse", learner = ll,
+    task = tsk("boston_housing"), ps = ll$param_set)
   test_res
 }
-
-
 
 # create inst object with dependencies
 PS_DEPS = function() {
@@ -180,13 +179,16 @@ mlr3::mlr_measures$add("dummy.cp.classif", MeasureDummyCPClassif)
 MeasureDummyCPRegr = make_dummy_cp_measure("regr")
 mlr3::mlr_measures$add("dummy.cp.regr", MeasureDummyCPRegr)
 
-LearnerRegrDepParams = R6Class("LearnerRegrDepParams", inherit = LearnerRegr,
+LearnerRegrDepParams = R6Class("LearnerRegrDepParams",
+  inherit = LearnerRegr,
   public = list(
     initialize = function(id = "regr.depparams") {
       param_set = PS_DEPS()
       super$initialize(
         id = id,
-        feature_types = c("logical", "integer", "numeric", "character", "factor", "ordered"),
+        feature_types = c(
+          "logical", "integer", "numeric", "character",
+          "factor", "ordered"),
         predict_types = c("response"),
         param_set = param_set,
         properties = c("missings")
