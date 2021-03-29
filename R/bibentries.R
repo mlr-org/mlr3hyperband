a4bc3ef404914302c6bc4e2f1279e59142a426d5
Marc Becker <marcbecker@posteo.de>
2021-03-29 11:48:50 +0200

    doc

--- R/bibentries.R
+++ R/bibentries.R
@@ -9,18 +9,5 @@ bibentries = c(
       number  = "185",
       pages   = "1-52",
       url     = "https://jmlr.org/papers/v18/16-558.html"
-  ),
-
-    jamieson_2016 = bibentry("InProceedings",
-      title     = 	 "Non-stochastic Best Arm Identification and Hyperparameter Optimization",
-      author    = 	 "Kevin Jamieson and Ameet Talwalkar",
-      booktitle =    "Proceedings of the 19th International Conference on Artificial Intelligence and Statistics",
-      pages     = 	 "240-248",
-      year      = 	 "2016",
-      editor    = 	 "Arthur Gretton and Christian C. Robert",
-      volume    = 	 "51",
-      series    = 	 "Proceedings of Machine Learning Research",
-      address   = 	 "Cadiz, Spain",
-      publisher =    "PMLR",
-      url       = "http://proceedings.mlr.press/v51/jamieson16.html")
+  )
 )
