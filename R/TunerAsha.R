82a8d4180009ceb4238d66f86980f1053e01d15a
be-marc <marcbecker@posteo.de>
2021-10-22 17:41:12 +0200

    asha

--- R/TunerAsha.R
+++ R/TunerAsha.R
@@ -11,6 +11,27 @@ TunerAsha = R6Class("TunerAsha",
       super$initialize(
         optimizer = OptimizerAsha$new()
       )
+    },
+
+    optimize = function(inst) {
+      assert_multi_class(inst, c("TuningInstanceSingleCrit", "TuningInstanceMultiCrit"))
+
+      # replace default with async objective
+      inst$objective = ObjectiveTuningAsync$new(task = inst$objective$task, learner = inst$objective$learner,
+        resampling = inst$objective$resampling, measures = inst$objective$measures,
+        store_benchmark_result = inst$objective$store_benchmark_result, store_models = inst$objective$store_models,
+        check_values = inst$objective$check_values, allow_hotstart = inst$objective$allow_hotstart)
+
+      private$.optimizer$optimize(inst)
+
+      # combine resample results to single benchmark result
+      if (!is.null(inst$archive$data$resample_result)) {
+        rdatas = map(unlist(inst$archive$data$resample_result), function(rr) get_private(rr)$.data)
+        rdata = Reduce(function(x, y) x$combine(y), rdatas)
+        inst$archive$benchmark_result = BenchmarkResult$new(rdata)
+        inst$archive$data["evaluated", uhash := inst$archive$benchmark_result$uhashes, on = "status"]
+        inst$archive$data[, resample_result := NULL]
+      }
     }
   )
 )
