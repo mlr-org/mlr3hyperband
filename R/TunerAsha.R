67622ff5355bf5891bccb6696a832478c406e534
be-marc <marcbecker@posteo.de>
2021-11-05 18:14:10 +0100

    simplify

--- R/TunerAsha.R
+++ R/TunerAsha.R
@@ -15,26 +15,8 @@ TunerAsha = R6Class("TunerAsha",
 
     optimize = function(inst) {
       assert_multi_class(inst, c("TuningInstanceSingleCrit", "TuningInstanceMultiCrit"))
-
-      # replace default with async objective
-      inst$objective = ObjectiveTuningAsync$new(task = inst$objective$task, learner = inst$objective$learner,
-        resampling = inst$objective$resampling, measures = inst$objective$measures,
-        store_benchmark_result = inst$objective$store_benchmark_result, store_models = inst$objective$store_models,
-        check_values = inst$objective$check_values, allow_hotstart = inst$objective$allow_hotstart)
-      inst$objective$hotstart_stack$learner_limit = 2000
-
+      inst$async = TRUE
       private$.optimizer$optimize(inst)
-
-      # combine resample results to single benchmark result
-      if (inst$objective$store_benchmark_result) {
-        rdatas = map(unlist(inst$archive$data$resample_result), function(rr) get_private(rr)$.data)
-        rdata = Reduce(function(x, y) x$combine(y), rdatas)
-        inst$archive$benchmark_result = BenchmarkResult$new(rdata)
-        inst$archive$data["evaluated", uhash := inst$archive$benchmark_result$uhashes, on = "status"]
-      }
-
-      # remove resample result column
-      if (!is.null(inst$archive$data$resample_result)) inst$archive$data[, resample_result := NULL]
     }
   )
 )
