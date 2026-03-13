e79ab6e81f5e9b7e639960fd2d0a884593803797
be-marc <marcbecker@posteo.de>
2026-03-13 15:32:37 +0100

    ...

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -8,10 +8,6 @@ old_opts = options(
 # https://github.com/HenrikBengtsson/Wishlist-for-R/issues/88
 old_opts = lapply(old_opts, function(x) if (is.null(x)) FALSE else x)
 
-lg_bbotk = lgr::get_logger("bbotk")
-old_threshold_bbotk = lg_bbotk$threshold
-lg_bbotk$set_threshold("warn")
-
 lg_mlr3 = lgr::get_logger("mlr3")
 old_threshold_mlr3 = lg_mlr3$threshold
 lg_mlr3$set_threshold("warn")
--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -1,3 +1,2 @@
 options(old_opts)
-lg_bbotk$set_threshold(old_threshold_bbotk)
 lg_mlr3$set_threshold(old_threshold_mlr3)
